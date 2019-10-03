9c5c5c311ff7f5a0ba85eb3258683be74e89d665
Macarena15 <aleksandra.by92@gmail.com>
2019-10-03 18:31:25 +0300

    feat: add decoder

--- src/index.js
+++ src/index.js
@@ -39,6 +39,33 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+        var input = '';
+
+    for (var i = 0; i < expr.length; i += 10) {
+        var mExpr = '';
+        var letter = expr.slice(i, i+10);
+        if (letter === '**********') {
+            input += ' ';
+            continue;
+        }
+
+        for (var j = 0; j < letter.length; j += 2) {
+            var mSymbol = '';
+            var symbol = letter.slice(j, j+2);
+
+            if (symbol === '10') {
+                mSymbol += '.';
+            } else if (symbol === '11') {
+                mSymbol += '-';
+            } else {
+                continue;
+            }
+            mExpr += mSymbol;
+        }
+
+        input += MORSE_TABLE[mExpr];
+    }
+    return input;
 }
 
 module.exports = {
